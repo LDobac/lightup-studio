0548fa46cd41b46130d18baa43a669b8ec0143b4
yikolden <ljh34210329@gmail.com>
2022-05-16 17:15:12 +0900

    등록된 게임 오브젝트의 게임 모듈에서 노출된 값 조회 기능 구현

--- src/studio/__test__/core/GameObjectManagerTest.spec.ts
+++ src/studio/__test__/core/GameObjectManagerTest.spec.ts
@@ -1,16 +1,292 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, afterEach } from "vitest";
+import GameModuleRegistry from "@/studio/core/GameModuleRegistry";
+import MockCompiler from "./MockCompiler";
+import GameObjectManager, {
+  GameObjectDuplicatedError,
+  GameObjectNotFoundError,
+} from "@/studio/core/GameObjectManager";
+import GameObject from "@/studio/core/runtime/GameObject";
+
+const compiler = new MockCompiler();
+const gameModuleRegistry = new GameModuleRegistry(compiler);
+
+const counterModule = await gameModuleRegistry.RegisterBySource(
+  "Counter",
+  [
+    "class counter extends Lib.GameModule {",
+    "   @Lib.Expose()",
+    "   public count : number = 0;",
+    "   public Start() { this.count = 10; }",
+    "   public Update(deltaTime : number) { this.Count(); }",
+    "   @Lib.Expose()",
+    "   public Count() : number {this.count++; return this.count;}",
+    "}",
+  ].join("\n")
+);
+
+const countAModule = await gameModuleRegistry.RegisterBySource(
+  "CountA",
+  [
+    "class counta extends Lib.GameModule {",
+    "   @Lib.Expose()",
+    "   public a : string = '';",
+    "   public Start() { this.a = 'start' }",
+    "   public Update(deltaTime : number) { this.GetA() }",
+    "   @Lib.Expose()",
+    "   public GetA() : string {this.a += 'a'; return this.a;}",
+    "}",
+  ].join("\n")
+);
 
 describe("GameObjectManagerTest", () => {
-  it("T", () => {
-    expect(true).toBeFalsy();
+  let gameObjectManager = new GameObjectManager();
+
+  afterEach(() => {
+    gameObjectManager = new GameObjectManager();
   });
 
-  // Create GameObject Test
-  // Check Duplicated
+  it("Create GameObject Test", () => {
+    const newGameObject = gameObjectManager.CreateGameObject();
 
-  // Remove GameObject Test
+    expect(newGameObject).toBeDefined();
+    expect(newGameObject.prototypeGameModule.length).toEqual(0);
 
-  // Get GameObject Test
+    expect(gameObjectManager.gameObjects.length).toEqual(1);
+  });
 
-  // Query Types Test
+  it("Create GameObject multiple times", () => {
+    for (let i = 0; i < 10; i++) {
+      const newGameObject = gameObjectManager.CreateGameObject();
+
+      expect(newGameObject).toBeDefined();
+      expect(newGameObject.prototypeGameModule.length).toEqual(0);
+
+      expect(gameObjectManager.gameObjects.length).toEqual(i + 1);
+    }
+  });
+
+  it("Create Gameobject given id Test", () => {
+    const expectId = "Given GameObject Id";
+
+    const newGameObject = gameObjectManager.CreateGameObject(expectId);
+
+    expect(newGameObject.id).toEqual(expectId);
+
+    expect(gameObjectManager.gameObjects.length).toEqual(1);
+  });
+
+  it("Add GameObject test", () => {
+    const newGameObject = new GameObject();
+
+    const gameObject = gameObjectManager.AddGameObject(newGameObject);
+
+    expect(gameObject).toEqual(newGameObject);
+
+    expect(gameObjectManager.gameObjects.length).toEqual(1);
+  });
+
+  it("Add GameObject Duplicate Test", () => {
+    const newGameObject = new GameObject();
+
+    gameObjectManager.AddGameObject(newGameObject);
+    expect(gameObjectManager.gameObjects.length).toEqual(1);
+
+    expect(() => gameObjectManager.AddGameObject(newGameObject)).toThrow(
+      GameObjectDuplicatedError
+    );
+    expect(gameObjectManager.gameObjects.length).toEqual(1);
+  });
+
+  it("RemoveGameObject Test", () => {
+    const newGameObject = gameObjectManager.CreateGameObject();
+    expect(gameObjectManager.gameObjects.length).toEqual(1);
+
+    gameObjectManager.RemoveGameObject(newGameObject);
+    expect(gameObjectManager.gameObjects.length).toEqual(0);
+  });
+
+  it("RemoveGameObject twice to same object", () => {
+    const newGameObject = gameObjectManager.CreateGameObject();
+
+    gameObjectManager.RemoveGameObject(newGameObject);
+    expect(gameObjectManager.gameObjects.length).toEqual(0);
+
+    expect(() => gameObjectManager.RemoveGameObject(newGameObject)).toThrow(
+      GameObjectNotFoundError
+    );
+  });
+
+  it("RemoveGameObject Not exists module", () => {
+    gameObjectManager.CreateGameObject();
+
+    const notExistsObject = new GameObject();
+
+    expect(() => gameObjectManager.RemoveGameObject(notExistsObject)).toThrow(
+      GameObjectNotFoundError
+    );
+  });
+
+  it("RemoveGameObjectById Test", () => {
+    const newGameObject = gameObjectManager.CreateGameObject();
+    expect(gameObjectManager.gameObjects.length).toEqual(1);
+
+    gameObjectManager.RemoveGameObjectById(newGameObject.id);
+    expect(gameObjectManager.gameObjects.length).toEqual(0);
+  });
+
+  it("RemoveGameObjectById twice to same object", () => {
+    const newGameObject = gameObjectManager.CreateGameObject();
+
+    gameObjectManager.RemoveGameObjectById(newGameObject.id);
+    expect(gameObjectManager.gameObjects.length).toEqual(0);
+
+    expect(() =>
+      gameObjectManager.RemoveGameObjectById(newGameObject.id)
+    ).toThrow(GameObjectNotFoundError);
+  });
+
+  it("RemoveGameObjectById Not exists module", () => {
+    gameObjectManager.CreateGameObject();
+
+    const notExistsObject = new GameObject();
+
+    expect(() =>
+      gameObjectManager.RemoveGameObjectById(notExistsObject.id)
+    ).toThrow(GameObjectNotFoundError);
+  });
+
+  it("GetGameObjectById Test", () => {
+    const newGameObject = gameObjectManager.CreateGameObject();
+
+    const findGameObject = gameObjectManager.GetGameObjectById(
+      newGameObject.id
+    );
+
+    expect(newGameObject).toEqual(findGameObject);
+  });
+
+  it("GetGameObjectById Not exists test", () => {
+    gameObjectManager.CreateGameObject();
+
+    expect(() =>
+      gameObjectManager.GetGameObjectById("Not exists game module id ")
+    ).toThrow(GameObjectNotFoundError);
+  });
+
+  it("Query Types only single game object Test 1", () => {
+    const newGameObject = gameObjectManager.CreateGameObject();
+
+    newGameObject.AddPrototypeGameModule(counterModule);
+
+    const exposeList = gameObjectManager.QueryExposeData(Number);
+
+    expect(exposeList.length).toEqual(1);
+
+    const exposeMetadata = exposeList[0];
+    expect(exposeMetadata.gameObjectId).toEqual(newGameObject.id);
+    expect(exposeMetadata.modules.length).toEqual(1);
+
+    expect(Object.keys(exposeMetadata.modules[0].metadata).length).toEqual(1);
+    expect(Object.keys(exposeMetadata.modules[0].metadata)[0]).toEqual("count");
+
+    expect(exposeMetadata.modules[0].metadata["count"].type).toEqual(Number);
+
+    const addedPGM = newGameObject.prototypeGameModule.find(
+      (v) => v.uid === exposeMetadata.modules[0].gameModuleUid
+    );
+    expect(addedPGM?.module).toEqual(counterModule);
+  });
+
+  it("Query Types only single game object Test 2", () => {
+    const newGameObject = gameObjectManager.CreateGameObject();
+
+    newGameObject.AddPrototypeGameModule(counterModule);
+
+    const exposeList = gameObjectManager.QueryExposeData(Function);
+
+    expect(exposeList.length).toEqual(1);
+
+    const exposeMetadata = exposeList[0];
+    expect(exposeMetadata.gameObjectId).toEqual(newGameObject.id);
+    expect(exposeMetadata.modules.length).toEqual(1);
+
+    expect(Object.keys(exposeMetadata.modules[0].metadata).length).toEqual(1);
+    expect(Object.keys(exposeMetadata.modules[0].metadata)[0]).toEqual("Count");
+
+    expect(exposeMetadata.modules[0].metadata["Count"].type).toEqual(Function);
+
+    const addedPGM = newGameObject.prototypeGameModule.find(
+      (v) => v.uid === exposeMetadata.modules[0].gameModuleUid
+    );
+    expect(addedPGM?.module).toEqual(counterModule);
+  });
+
+  it("Query types multiple object Test", () => {
+    const gameObject1 = gameObjectManager.CreateGameObject();
+    const gameObject2 = gameObjectManager.CreateGameObject();
+
+    gameObject1.AddPrototypeGameModule(counterModule);
+    gameObject1.AddPrototypeGameModule(countAModule);
+
+    gameObject2.AddPrototypeGameModule(counterModule);
+    gameObject2.AddPrototypeGameModule(countAModule);
+
+    const exposeList = gameObjectManager.QueryExposeData(Number);
+
+    expect(exposeList.length).toEqual(2);
+
+    for (const expose of exposeList) {
+      for (const module of expose.modules) {
+        expect(Object.keys(module.metadata).length).toEqual(1);
+
+        for (const metadata of Object.values(module.metadata)) {
+          expect(metadata.type).toEqual(Number);
+        }
+      }
+    }
+  });
+
+  it("Query Types ignore test 1", () => {
+    const newGameObject = gameObjectManager.CreateGameObject();
+
+    newGameObject.AddPrototypeGameModule(counterModule);
+
+    const exposeList = gameObjectManager.QueryExposeData(Number, [newGameObject]);
+
+    expect(exposeList.length).toEqual(0);
+  });
+
+  it("Query Types ignore test 2", () => {
+    const gameObject1 = gameObjectManager.CreateGameObject();
+    const gameObject2 = gameObjectManager.CreateGameObject();
+
+    gameObject1.AddPrototypeGameModule(counterModule);
+    gameObject1.AddPrototypeGameModule(countAModule);
+
+    gameObject2.AddPrototypeGameModule(counterModule);
+    gameObject2.AddPrototypeGameModule(countAModule);
+
+    const exposeList = gameObjectManager.QueryExposeData(Number, [gameObject2]);
+
+    expect(exposeList.length).toEqual(1);
+  });
+
+  it("Query Types ignore test 3", () => {
+    const gameObject1 = gameObjectManager.CreateGameObject();
+    const gameObject2 = gameObjectManager.CreateGameObject();
+
+    gameObject1.AddPrototypeGameModule(counterModule);
+    gameObject1.AddPrototypeGameModule(countAModule);
+
+    gameObject2.AddPrototypeGameModule(counterModule);
+    gameObject2.AddPrototypeGameModule(countAModule);
+
+    const exposeList = gameObjectManager.QueryExposeData(Number, [gameObject1, gameObject2]);
+
+    expect(exposeList.length).toEqual(0);
+  });
+
+  // TODO
+  // Get Value by query types when game started
+  // Have to failed getting get value by query types when game does not started.
 });
--- src/studio/core/GameObjectManager.ts
+++ src/studio/core/GameObjectManager.ts
@@ -1,4 +1,5 @@
 import type GameModuleRegistry from "./GameModuleRegistry";
+import type { IExposeMetadata } from "./runtime/ExposeDecorator";
 import GameObject from "./runtime/GameObject";
 
 export class GameObjectNotFoundError extends Error {
@@ -13,12 +14,25 @@ export class GameObjectDuplicatedError extends Error {
   }
 }
 
+export interface IExposedValue {
+  gameObjectId: string;
+
+  modules: Array<{
+    gameModuleUid: string;
+    metadata: IExposeMetadata;
+  }>;
+}
+
 export default class GameObjectManager {
   // NOTE : 최적화를 위해서 gameObjects 변수를 Key:Value 컨테이너로 교체?
   private _gameObjects: Array<GameObject>;
 
+  private running: boolean;
+
   public constructor() {
     this._gameObjects = [];
+
+    this.running = false;
   }
 
   public CreateGameObject(id = ""): GameObject {
@@ -67,8 +81,52 @@ export default class GameObjectManager {
     this._gameObjects = [];
   }
 
-  public QueryExposeData(type: string) {
-    throw "Not Implement.";
+  public QueryExposeData(
+    type: unknown,
+    ignoreList: GameObject[] = []
+  ): IExposedValue[] {
+    const exposedValues: IExposedValue[] = [];
+
+    for (const gameObject of this._gameObjects) {
+      if (ignoreList.includes(gameObject)) {
+        continue;
+      }
+
+      const exposeValue: IExposedValue = {
+        gameObjectId: gameObject.id,
+        modules: [],
+      };
+
+      // NOTE : 최적화 필요시 별도의 metadata 값 생성 수 type으로 해시 테이블?
+      for (const gameModule of gameObject.prototypeGameModule) {
+        const exposeMetadata: IExposeMetadata = {};
+
+        const metadata = gameModule.module.exposeMetadata;
+
+        for (const propertyName of Object.keys(metadata)) {
+          if (
+            metadata[propertyName].type === type ||
+            (metadata[propertyName].type as object).toString().toLowerCase() ===
+              (type as object).toString().toLowerCase()
+          ) {
+            exposeMetadata[propertyName] = metadata[propertyName];
+          }
+        }
+
+        if (Object.keys(exposeMetadata).length > 0) {
+          exposeValue.modules.push({
+            gameModuleUid: gameModule.uid,
+            metadata: exposeMetadata,
+          });
+        }
+      }
+
+      if (exposeValue.modules.length > 0) {
+        exposedValues.push(exposeValue);
+      }
+    }
+
+    return exposedValues;
   }
 
   public GameSetup(gameModuleRegistry: GameModuleRegistry) {
@@ -78,6 +136,8 @@ export default class GameObjectManager {
   }
 
   public GameStart() {
+    this.running = true;
+
     this._gameObjects.forEach((go) => {
       go.Start();
     });
@@ -90,6 +150,8 @@ export default class GameObjectManager {
   }
 
   public GameFinish() {
+    this.running = false;
+
     this._gameObjects.forEach((go) => {
       go.Finish();
     });
